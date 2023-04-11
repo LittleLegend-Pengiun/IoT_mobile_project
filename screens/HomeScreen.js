ec77390c877eacc99ecb09fb03402cbf0ac95861
LittleLegend-Pengiun <hoanganhleboy@gmail.com>
2023-04-11 10:01:57 +0700

    Initial commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Test from '../components/Test'
+
+
+const HomeScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Home Screen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
