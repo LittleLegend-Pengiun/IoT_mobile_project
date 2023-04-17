426ecfa86fd1d7effcb0bbf6ade8c6f316e62d81
LittleLegend-Pengiun <hoanganhleboy@gmail.com>
2023-04-17 23:25:52 +0700

    Split app file

---
+++ IotApp.js
@@ -0,0 +1,44 @@
+import HomeScreen from './screens/HomeScreen';
+import ChartScreen from './screens/ChartScreen';
+import HistoryScreen from './screens/HistoryScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
+
+export default function IotApp() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused
+              ? 'ios-home-sharp'
+              : 'ios-home-outline';
+          } else if (route.name === 'Charts') {
+            iconName = focused ? 'ios-bar-chart' : 'ios-bar-chart-outline';
+          } else if (route.name === 'History') {
+            iconName = focused ? 'ios-documents' : 'ios-documents-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'green',
+        tabBarInactiveTintColor: 'gray',
+      })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} 
+        options={{presentation: "transparentModal", headerShown: false}}/>
+        <Tab.Screen name="Charts" component={ChartScreen} 
+        options={{presentation: "transparentModal", headerShown: false}}/>
+        <Tab.Screen name="History" component={HistoryScreen} 
+        options={{presentation: "transparentModal", headerShown: false}}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
